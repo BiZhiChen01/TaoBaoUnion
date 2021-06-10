5b7207194e64593528d097326cb52d5943d448d4
BiZhiChen01 <1973626642@qq.com>
2021-06-10 23:31:54 +0800

    添加底部導航欄，實現頁面切換

--- app/src/main/java/com/chen/taobaounion/ui/activity/MainActivity.java
+++ app/src/main/java/com/chen/taobaounion/ui/activity/MainActivity.java
@@ -1,16 +1,77 @@
 package com.chen.taobaounion.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.chen.taobaounion.R;
+import com.chen.taobaounion.base.BaseFragment;
+import com.chen.taobaounion.ui.fragment.HomeFragment;
+import com.chen.taobaounion.ui.fragment.RedPacketFragment;
+import com.chen.taobaounion.ui.fragment.SearchFragment;
+import com.chen.taobaounion.ui.fragment.SelectedFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jetbrains.annotations.NotNull;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_nav_bar)
+    public BottomNavigationView mNavigationView;
+    private HomeFragment mHomeFragment;
+    private SelectedFragment mSelectedFragment;
+    private RedPacketFragment mRedPacketFragment;
+    private SearchFragment mSearchFragment;
+    private FragmentManager mManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        initFragment();
+        initListener();
+    }
+
+    private void initFragment() {
+        mHomeFragment = new HomeFragment();
+        mSelectedFragment = new SelectedFragment();
+        mRedPacketFragment = new RedPacketFragment();
+        mSearchFragment = new SearchFragment();
+
+        mManager = getSupportFragmentManager();
+        switchFragment(mHomeFragment);
+    }
+
+    private void initListener() {
+        mNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull @NotNull MenuItem item) {
+                if (item.getItemId() == R.id.home) {
+                    switchFragment(mHomeFragment);
+                } else if (item.getItemId() == R.id.selected) {
+                    switchFragment(mSelectedFragment);
+                } else if (item.getItemId() == R.id.red_packet) {
+                    switchFragment(mRedPacketFragment);
+                } else if (item.getItemId() == R.id.search) {
+                    switchFragment(mSearchFragment);
+                }
+                return true;
+            }
+        });
+    }
+
+    private void switchFragment(BaseFragment targetFragment) {
+        FragmentTransaction transaction = mManager.beginTransaction();
+        transaction.replace(R.id.main_page_content, targetFragment);
+        transaction.commit();
     }
 }
---
+++ app/src/main/java/com/chen/taobaounion/ui/fragment/HomeFragment.java
@@ -0,0 +1,12 @@
+package com.chen.taobaounion.ui.fragment;
+
+import com.chen.taobaounion.R;
+import com.chen.taobaounion.base.BaseFragment;
+
+public class HomeFragment extends BaseFragment {
+
+    @Override
+    protected int getRootViewResId() {
+        return R.layout.fragment_home;
+    }
+}
